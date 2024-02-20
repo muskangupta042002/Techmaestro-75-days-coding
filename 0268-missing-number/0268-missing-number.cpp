39866be466519020ca578c2e2dae4a3fe37ff910
Muskan Gupta <59511430+muskangupta042002@users.noreply.github.com>
2024-02-20 23:25:57 +0530

    Time: 11 ms (85.38%), Space: 20.8 MB (16.09%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int miss=-1;
+        int n=nums.size();
+        vector<int> arr(n+1,0);
+        for(int i=0;i<n;i++){
+            arr[nums[i]]=1;
+        }
+        for(int i=0;i<n+1;i++){
+            if(arr[i]==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
