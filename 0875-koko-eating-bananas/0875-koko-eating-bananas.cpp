be6c9b687f363e4d44416260b00bb9611ebd7eb2
Muskan Gupta <59511430+muskangupta042002@users.noreply.github.com>
2024-03-03 01:10:20 +0530

    Time: 36 ms (44.46%), Space: 21.4 MB (23.81%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,41 +1,25 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-//         int maxN=*max_element(piles.begin(),piles.end());
-//         int i=0;
-//         //long long int ans=1000000009;
-//         while(i<=maxN){
-//             int mid=(i+maxN)/2;
-//             long sum=0;
-//             for(int i=0;i<piles.size();i++){
-//                 sum +=ceil((double)piles[i] / (double)mid);
-//             }
-//             if(sum<=h){
-                
-//                 maxN=mid-1;
-//             }
-//             else{
-//                 i=mid+1;
-//             }
-            
-            
-//         }
-//         return i;
-         auto it = max_element(piles.begin(), piles.end());
-        int low = 1, high = *it;
-        cout<<high;
-        while (low <= high) {
-            long hrs = 0;
-            int mid = (low + high) / 2;
-            for (int i = 0; i < piles.size(); i++) {
-                hrs += ceil((double)piles[i] / (double)mid);
+        int maxN=*max_element(piles.begin(),piles.end());
+        int i=1;
+        //long long int ans=1000000009;
+        while(i<=maxN){
+            int mid=(i+maxN)/2;
+            long sum=0;
+            for(int j=0;j<piles.size();j++){
+                sum +=ceil((double)piles[j] / (double)mid);
             }
-            if (hrs <= h) {
-                high = mid - 1;
-
-            } else
-                low = mid + 1;
+            if(sum<=h){
+                
+                maxN=mid-1;
+            }
+            else{
+                i=mid+1;
+            }
+            
+            
         }
-        return low;
+        return i;
     }
 };
