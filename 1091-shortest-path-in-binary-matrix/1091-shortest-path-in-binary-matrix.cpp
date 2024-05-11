1e30d0e5968ef785a32d275747652e59c687fccd
Muskan Gupta <59511430+muskangupta042002@users.noreply.github.com>
2024-05-12 00:07:41 +0530

    Time: 47 ms (57.76%), Space: 25 MB (47.17%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -5,7 +5,7 @@ public:
            if(grid[0][0]==0 && n==1) return 1;
         pair<int,int> src{0,0};
     pair<int,int> dest{n-1,n-1};
-    if(src.first==dest.first && src.second==dest.second && grid[src.first][src.second]==0) return 0;
+    
     if(src.first==dest.first && src.second==dest.second && grid[src.first][src.second]==1) return -1;
     if(grid[src.first][src.second]==1 || grid[dest.first][dest.second]==1) return -1;
         vector<vector<int>> ans(n,vector<int>(n,1e9));
