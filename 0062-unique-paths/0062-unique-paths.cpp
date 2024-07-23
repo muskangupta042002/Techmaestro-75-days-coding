8180099535ad33a71742bebe2927311bf492b92a
Muskan Gupta <59511430+muskangupta042002@users.noreply.github.com>
2024-07-23 19:01:16 +0530

    Time: 3 ms (33.89%), Space: 8 MB (5.25%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -15,12 +15,18 @@ public:
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return helper(m-1,n-1,dp);
-        // for(int i=0;i<m;i++){
-        //     for(int j=0;j<n;j++){
-        //         dp[i][j]=dp[i-1][j]+dp[i][j-1];
-        //     }
-        // }
-        // return dp[0][0];
+        //return helper(m-1,n-1,dp);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                }
+                
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
