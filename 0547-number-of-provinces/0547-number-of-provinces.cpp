f088579414df2de3e94cecf84634e618077ae647
Muskan Gupta <59511430+muskangupta042002@users.noreply.github.com>
2024-07-31 14:33:23 +0530

    Time: 18 ms (80.5%), Space: 18.5 MB (63.48%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,25 +1,44 @@
 class Solution {
 public:
-    void dfs(int x,  int n, vector<vector<int>>& isConnected, vector<bool>& visit) {
-        
+    struct Node {
+        int parent;
+        int rank;
+    };
+    vector<Node> dsuf;
 
-        visit[x] = true;
-        for(int i=0;i<n;i++){
-            if(isConnected[x][i] && !visit[i]){
-                dfs(i,n,isConnected,visit);
-            }
+    int find(int v) {
+        if (dsuf[v].parent == -1) {
+            return v;
+        }
+        return dsuf[v].parent = find(dsuf[v].parent); // path compression
+    }
+
+    void union_op(int fromP, int toP) {
+        if (dsuf[fromP].rank > dsuf[toP].rank) {
+            dsuf[toP].parent = fromP;
+        } else if (dsuf[fromP].rank < dsuf[toP].rank) {
+            dsuf[fromP].parent = toP;
+        } else {
+            dsuf[fromP].parent = toP;
+            dsuf[toP].rank += 1;
         }
     }
 
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
-        vector<bool>visit(n,false);
-        int count=0;
-        for(int i=0;i<n;i++){
-            if(!visit[i]){
-                     dfs(i, n, isConnected, visit);
-                     count++;
-                
+        int n = isConnected.size();
+        // vector<bool> visit(n, false);
+        int count = n;
+        dsuf.resize(n);
+        for (int i = 0; i < n; ++i) {
+            dsuf[i].parent = -1; // Each node is its own parent initially
+            dsuf[i].rank = 0;    // Initial rank is 0
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isConnected[i][j] && find(i) != find(j)) {
+                    count--;
+                    union_op(find(i),find(j) );
+                }
             }
         }
         return count;
