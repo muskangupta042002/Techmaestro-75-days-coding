7a2f67632b02dd24ec5b75cb455f006e860c0a6e
Muskan Gupta <59511430+muskangupta042002@users.noreply.github.com>
2024-08-26 01:48:34 +0530

    Time: 379 ms (82.98%), Space: 183.9 MB (82.28%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.cpp
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -5,10 +5,10 @@ public:
         int key;
         int val;
         int used;
-        Node *prev, *next;
-        Node(int _key, int _val) {
-            key = _key;
-            val = _val;
+        Node *next, *prev;
+        Node(int k, int v) {
+            key = k;
+            val = v;
             used = 1;
         }
     };
@@ -16,84 +16,75 @@ public:
     class List {
     public:
         int size;
-        Node* head;
-        Node* tail;
+        Node *head, *tail;
         List() {
+            size = 0;
             head = new Node(-1, -1);
             tail = new Node(-1, -1);
             head->next = tail;
             tail->prev = head;
-            size = 0;
         }
-        void addNode(Node* newNode) {
+        void addNode(Node* node) {
             Node* headNext = head->next;
-            newNode->prev = head;
-            newNode->next = headNext;
-            headNext->prev = newNode;
-            head->next = newNode;
+            node->prev = head;
+            node->next = headNext;
+            head->next = node;
+            headNext->prev = node;
             size++;
         }
 
-        void deleteNode(Node* delNode) {
-            Node* Next = delNode->next;
-            Node* Prev = delNode->prev;
-
-            Prev->next = Next;
-            Next->prev = Prev;
+        void deleteNode(Node* node) {
+            Node* delNext = node->next;
+            Node* delPrev = node->prev;
+            delPrev->next = delNext;
+            delNext->prev = delPrev;
             size--;
         }
 
-        bool isEmpty() {
-            return size == 0;
-        }
+        bool isEmpty() { return size == 0; }
     };
-
+    int maxSize, minFreq, currSize;
     unordered_map<int, Node*> mp;
     unordered_map<int, List*> freqMap;
-    int maxSize, minFreq, currSize;
-    
     LFUCache(int capacity) {
         maxSize = capacity;
-        currSize = 0;
         minFreq = 0;
+        currSize = 0;
     }
 
     void updateFreqMap(Node* node) {
         mp.erase(node->key);
-        freqMap[node->used]->deleteNode(node);
 
+        freqMap[node->used]->deleteNode(node);
         if (node->used == minFreq && freqMap[node->used]->isEmpty()) {
             minFreq++;
         }
-
-        node->used += 1;
-
+        node->used++;
         if (freqMap.find(node->used) == freqMap.end()) {
             freqMap[node->used] = new List();
         }
-
         freqMap[node->used]->addNode(node);
         mp[node->key] = node;
     }
-
     int get(int key) {
-        if (mp.find(key) != mp.end()) {
-            Node* temp = mp[key];
-            updateFreqMap(temp);
-            return temp->val;
+        if (mp.find(key) == mp.end()) {
+            return -1;
         }
-        return -1;
+        Node* temp = mp[key];
+        updateFreqMap(temp);
+
+        return temp->val;
     }
 
     void put(int key, int value) {
         if (maxSize == 0) {
             return;
         }
-
         if (mp.find(key) != mp.end()) {
             Node* temp = mp[key];
             temp->val = value;
             updateFreqMap(temp);
+
         } else {
             if (currSize == maxSize) {
                 List* list = freqMap[minFreq];
@@ -101,17 +92,21 @@ public:
                 list->deleteNode(list->tail->prev);
                 currSize--;
             }
-
             currSize++;
             minFreq = 1;
-
             if (freqMap.find(minFreq) == freqMap.end()) {
                 freqMap[minFreq] = new List();
             }
-
             Node* node = new Node(key, value);
-            freqMap[minFreq]->addNode(node);
+            freqMap[node->used]->addNode(node);
             mp[key] = node;
         }
     }
 };
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache* obj = new LFUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
