7142c66361871283893101b6b7eaa33c8799b231
Muskan Gupta <59511430+muskangupta042002@users.noreply.github.com>
2024-08-25 23:59:06 +0530

    Time: 39 ms (76.44%), Space: 23 MB (66.3%) - LeetHub

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,38 +1,44 @@
 class Solution {
 public:
     int shortestPath(vector<vector<int>>& grid, int k) {
-        int m=grid.size(), n=grid[0].size();
-        if(n==1 && m==1) return 0;
+        int m = grid.size(), n = grid[0].size();
+        if (n == 1 && m == 1)
+            return 0;
         queue<vector<int>> q;
-        vector<vector<int>>vis(m,vector<int>(n,-1));
-        //i,j,visited towns,coinsleft
-        q.push({0,0,0,k});
-        vis[0][0]=k;
-        int count=0, mini=INT_MAX;
-        while(!q.empty()){
-            auto a=q.front();
-            int coins=a[3];
-            int towns=a[2];
-            int row=a[0];
-            int col=a[1];
+        vector<vector<int>> vis(m, vector<int>(n, -1));
+        // i,j,visited towns,coinsleft
+        q.push({0, 0, 0, k});
+        vis[0][0] = k;
+        int count = 0, mini = INT_MAX;
+        while (!q.empty()) {
+            auto a = q.front();
+            int coins = a[3];
+            int towns = a[2];
+            int row = a[0];
+            int col = a[1];
             q.pop();
-            int dx[4]={0,1,0,-1};
-            int dy[4]={1,0,-1,0};
-            if(row==m-1 && col==n-1){
+            int dx[4] = {0, 1, 0, -1};
+            int dy[4] = {1, 0, -1, 0};
+            if (row == m - 1 && col == n - 1) {
                 return towns;
             }
-            if(grid[row][col]==1){
-                if(!coins){
+            if (grid[row][col] == 1) {
+                // If we reach a town with toll plaza. If no coins left, we
+                // continue, else decrement coins.
+                if (!coins) {
                     continue;
                 }
                 coins--;
             }
-            for(int i=0;i<4;i++){
-                int r=row+dx[i];
-                int c=col+dy[i];
-                if(r>=0 && r<m && c>=0 && c<n && vis[r][c]<coins){
-                    q.push({r,c,towns+1,coins});
-                    vis[r][c]=coins;
+            for (int i = 0; i < 4; i++) {
+                int r = row + dx[i];
+                int c = col + dy[i];
+                // If cell lies in matrix, and has either not been visited, or
+                // visited wih less number of coins, we can push the cell in
+                // queue.
+                if (r >= 0 && r < m && c >= 0 && c < n && vis[r][c] < coins) {
+                    q.push({r, c, towns + 1, coins});
+                    vis[r][c] = coins;
                 }
             }
         }
