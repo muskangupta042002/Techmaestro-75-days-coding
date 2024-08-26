bc54f893f213f69cf55be620659fd42d8b0e3b2a
Muskan Gupta <59511430+muskangupta042002@users.noreply.github.com>
2024-08-27 00:50:39 +0530

    Time: 3 ms (58.88%), Space: 14 MB (33.68%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n=nums.size();
-        int i=0, j=n-1;
-        int index=-1;
-
-        while(i<=j){
-            int mid=i+((j-i)>>1);
-            if(nums[mid]==target){
+        int n = nums.size();
+        int i = 0, j = n - 1;
+        while (i <= j) {
+            int mid = (i + j) / 2;
+            cout << i << " " << mid << " " << j << ";";
+            if (nums[mid] == target) {
                 return mid;
             }
-            if(nums[i]==nums[mid] && nums[mid]==nums[j]){
-                i=i+1;
-                j=j-1; //reduce search space for edge case 3 2 3 3 3 if finding 2
-                continue;
-            }
-            if(nums[i]<=nums[mid]){
-                if(nums[i]<=target && target<=nums[mid]){
-                    j=mid-1;
+            if (nums[i] <= nums[mid]) {
+                if (nums[i] <= target && target <= nums[mid]) {
+                    cout<<"one ";
+                    j = mid - 1;
+                } else {
+                    cout<<"two ";
+                    i = mid + 1;
                 }
-                else{ i=mid+1;}
-            }
-            else {
-                if(nums[mid]<=target && target<=nums[j]){
-                    i=mid+1;
+
+            } else {
+                if (nums[mid] <= target && target <= nums[j]) {
+                    cout<<"three ";
+                    i = mid + 1;
+                } else {
+                    cout<<"four ";
+                    j = mid - 1;
                 }
-                else{ j=mid-1;}
             }
         }
-        return index;
+        return -1;
     }
 };
