090cf2498f3fd5b0d6a3969ccac5895084a7aec4
Muskan Gupta <59511430+muskangupta042002@users.noreply.github.com>
2024-08-29 00:17:50 +0530

    Time: 5 ms (60.16%), Space: 16 MB (43.6%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -23,22 +23,39 @@ public:
         int n = prices.size();
         if (n == 1)
             return 0;
-        vector<vector<int>> dp(n + 1, vector<int>(2, -1));
+        //vector<vector<int>> dp(n + 1, vector<int>(2, -1));
         // return helper(0, 1, prices, dp);
-        dp[n][0] = 0, dp[n][1] = 0;
+        //dp[n][0] = 0, dp[n][1] = 0;
+        vector<int> prev(2,0), curr(2,0);
+        //prev[0]=0, prev[1]
         for (int i = n - 1; i >= 0; i--) {
             for (int buy = 0; buy < 2; buy++) {
                 if (buy) {
-                    int buying = -prices[i] + dp[i+1][0];
-                    int notBuy =  dp[i+1][1];
-                    dp[i][buy] = max(buying, notBuy);
+                    int buying = -prices[i] +prev[0];
+                    int notBuy =  prev[1];
+                    curr[buy] = max(buying, notBuy);
                 } else {
-                    int sell = prices[i] + dp[i+1][1];
-                    int notSell = dp[i+1][0];
-                    dp[i][buy] = max(sell, notSell);
+                    int sell = prices[i] + prev[1];
+                    int notSell = prev[0];
+                    curr[buy] = max(sell, notSell);
                 }
             }
+            prev=curr;
         }
-        return dp[0][1];
+        return prev[1];
+        // for (int i = n - 1; i >= 0; i--) {
+        //     for (int buy = 0; buy < 2; buy++) {
+        //         if (buy) {
+        //             int buying = -prices[i] + dp[i+1][0];
+        //             int notBuy =  dp[i+1][1];
+        //             dp[i][buy] = max(buying, notBuy);
+        //         } else {
+        //             int sell = prices[i] + dp[i+1][1];
+        //             int notSell = dp[i+1][0];
+        //             dp[i][buy] = max(sell, notSell);
+        //         }
+        //     }
+        // }
+       // return dp[0][1];
     }
 };
