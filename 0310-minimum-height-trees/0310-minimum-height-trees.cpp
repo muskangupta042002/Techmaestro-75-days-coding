df9bfd85f34f353a3dc1884e3a634eaa9c668190
Muskan Gupta <59511430+muskangupta042002@users.noreply.github.com>
2024-08-28 22:18:27 +0530

    Time: 129 ms (30.46%), Space: 58.5 MB (57.13%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.cpp
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -1,39 +1,62 @@
 class Solution {
 public:
-    
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        if(n==1){
+        if (n == 1) {
             return {0};
         }
-        vector<int> ans;
+        
         vector<int> adj[n];
-        for(auto it: edges){
-            int u=it[0];
-            int v=it[1];
+        vector<int> indegree(n, 0);
+        for (auto it : edges) {
+            int u = it[0];
+            int v = it[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
-
+            indegree[u]++;
+            indegree[v]++;
         }
-        vector<int> leaves;
-        for(int i=0;i<n;i++){
-            int s=adj[i].size();
-            if(s==1){
-                leaves.push_back(i);
-            }
+        queue<int> q;
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 1)
+                q.push(i), indegree[i]--;
         }
-        
-        while(n>2){
-            n-=leaves.size();
-            vector<int> newLeaves;
-            for(int leaf: leaves){
-                int nextNode = *(adj[leaf].begin());
-                std::erase(adj[nextNode],leaf);
-                if(adj[nextNode].size()==1){
-                    newLeaves.push_back(nextNode);
+        vector<int> ans;
+        while (!q.empty()) {
+            int s = q.size();
+            ans.clear();
+            for (int i = 0; i < s; i++) {
+                int curr = q.front();
+                q.pop();
+                ans.push_back(curr);
+                for (auto child : adj[curr]) {
+                    indegree[child]--;
+                    if (indegree[child] == 1) {
+                        q.push(child);
+                    }
                 }
             }
-            leaves=newLeaves;
         }
-        return leaves;
+        return ans;
+        // vector<int> leaves;
+        // for(int i=0;i<n;i++){
+        //     int s=adj[i].size();
+        //     if(s==1){
+        //         leaves.push_back(i);
+        //     }
+        // }
+
+        // while(n>2){
+        //     n-=leaves.size();
+        //     vector<int> newLeaves;
+        //     for(int leaf: leaves){
+        //         int nextNode = *(adj[leaf].begin());
+        //         std::erase(adj[nextNode],leaf);
+        //         if(adj[nextNode].size()==1){
+        //             newLeaves.push_back(nextNode);
+        //         }
+        //     }
+        //     leaves=newLeaves;
+        // }
+       // return leaves;
     }
 };
