c1b6fc2c0bb2b2008e15489ed5871852f4e5338b
Muskan Gupta <59511430+muskangupta042002@users.noreply.github.com>
2024-08-29 00:08:08 +0530

    Time: 12 ms (14.68%), Space: 17.9 MB (30.67%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -21,12 +21,24 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        if(n==1) return 0;
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        return helper(0, 1, prices, dp);
-        // dp[n]
-        // for(int i=0;i<n;i++){
-
-        // }
+        if (n == 1)
+            return 0;
+        vector<vector<int>> dp(n + 1, vector<int>(2, -1));
+        // return helper(0, 1, prices, dp);
+        dp[n][0] = 0, dp[n][1] = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 0; buy < 2; buy++) {
+                if (buy) {
+                    int buying = -prices[i] + dp[i+1][0];
+                    int notBuy =  dp[i+1][1];
+                    dp[i][buy] = max(buying, notBuy);
+                } else {
+                    int sell = prices[i] + dp[i+1][1];
+                    int notSell = dp[i+1][0];
+                    dp[i][buy] = max(sell, notSell);
+                }
+            }
+        }
+        return dp[0][1];
     }
 };
