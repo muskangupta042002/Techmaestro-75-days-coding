1fcc3eea605d5a7a7f45fd28769cb5189d2990cd
Muskan Gupta <59511430+muskangupta042002@users.noreply.github.com>
2024-08-29 23:41:56 +0530

    Time: 12 ms (49.36%), Space: 24.8 MB (35.27%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n=height.size();
-        int ans=0;
-        vector<int> preMax(n,0), postMax(n,0);
-        preMax[0]=height[0], postMax[n-1]=height[n-1];
-        for(int i=1;i<n;i++){
-            preMax[i]=max(preMax[i-1],height[i]);
-        }
-        for(int i=n-2;i>=0;i--){
-            postMax[i]=max(postMax[i+1],height[i]);
-        }
-        for(int i=0;i<n;i++){
-            int temp=min(preMax[i],postMax[i]);
-            ans+=temp-height[i];
+        int l = 0, r = height.size() - 1;
+        int lm = height[l], rm = height[r];
+        int ans = 0;
+        while(l < r) {
+            if(lm > rm) {
+                ans += rm - height[r];
+                r--;
+                rm = max(rm, height[r]);
+            } else {
+                ans += lm - height[l];
+                l++;
+                lm = max(lm, height[l]);
+            }
         }
         return ans;
+        // int n=height.size();
+        // int ans=0;
+        // vector<int> preMax(n,0), postMax(n,0);
+        // preMax[0]=height[0], postMax[n-1]=height[n-1];
+        // for(int i=1;i<n;i++){
+        //     preMax[i]=max(preMax[i-1],height[i]);
+        // }
+        // for(int i=n-2;i>=0;i--){
+        //     postMax[i]=max(postMax[i+1],height[i]);
+        // }
+        // for(int i=0;i<n;i++){
+        //     int temp=min(preMax[i],postMax[i]);
+        //     ans+=temp-height[i];
+        // }
+        // return ans;
     }
 };
 /*
