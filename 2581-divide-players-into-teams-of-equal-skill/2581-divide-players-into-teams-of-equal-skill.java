3de783e62f3662757860b3152f19c3c4cd9d3198
Muskan Gupta <59511430+muskangupta042002@users.noreply.github.com>
2024-10-04 11:12:36 +0530

    Time: 3 ms (96.47%), Space: 55.8 MB (93.45%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.java
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.java
@@ -1,18 +1,41 @@
 class Solution {
     public long dividePlayers(int[] skill) {
-        Arrays.sort(skill);
-        int i=1, n=skill.length;
-        int j=n-2;
-        long sum=skill[0]+skill[n-1];
-        long ans=skill[0]*skill[n-1];
-        while(i<j){
-            long k=skill[i]+skill[j];
-            if(k!=sum){
+        // Arrays.sort(skill);
+        // int i=1, n=skill.length;
+        // int j=n-2;
+        // long sum=skill[0]+skill[n-1];
+        // long ans=skill[0]*skill[n-1];
+        // while(i<j){
+        //     long k=skill[i]+skill[j];
+        //     if(k!=sum){
+        //         return -1;
+        //     }
+        //     ans+=skill[i]*skill[j];
+        //     i++; j--;
+        // }
+        // return ans;
+
+        //Optmized 
+        int n=skill.length;
+        int totalSum=0;
+        int[] skillFreq = new int[1001];
+        for(int i: skill){
+            totalSum+=i;
+            skillFreq[i]++;
+        }
+
+        if(totalSum%(n/2) != 0) return -1;
+        int target=((totalSum)/(n/2));
+        long ans=0;
+        for(int i: skill){
+            int k=target-i;
+            if(skillFreq[k]==0){
                 return -1;
             }
-            ans+=skill[i]*skill[j];
-            i++; j--;
+            ans+=(long)k* (long)i;
+            skillFreq[k]--;
         }
-        return ans;
+        //count pairs twice
+        return ans/2;
     }
 }
