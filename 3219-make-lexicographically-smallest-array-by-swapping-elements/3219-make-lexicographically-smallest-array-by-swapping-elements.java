be7e84c548e5f6a17a060374701b7a95ac52dbd4
Muskan Gupta <59511430+muskangupta042002@users.noreply.github.com>
2025-01-26 02:19:28 +0530

    Time: 75 ms (85.42%), Space: 64.4 MB (77.08%) - LeetHub

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/3219-make-lexicographically-smallest-array-by-swapping-elements.java
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/3219-make-lexicographically-smallest-array-by-swapping-elements.java
@@ -3,7 +3,9 @@ class Solution {
         ArrayList<int []> arr = new ArrayList<>();
         int n=nums.length;
         for(int i=0;i<n;i++){
-            arr.add(new int[]{nums[i], i});
+            arr.add(new int[2]);
+            arr.get(i)[0]=nums[i];
+            arr.get(i)[1]=i;
         }
         Collections.sort(arr, Comparator.comparingInt(i -> i[0]));
         int i=0, j=1;
@@ -15,7 +17,7 @@ class Solution {
                 j++;
             }
             Collections.sort(indexes);
-            for(int k=0;k<indexes.size();k++){
+            for(int k=0;k<j-i;k++){
                 nums[indexes.get(k)]=arr.get(i+k)[0];
             }
             i=j;
